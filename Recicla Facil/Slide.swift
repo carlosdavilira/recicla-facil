253cb102917c59053465bb6e2db407ce68b03e6d
Carlos David <carlosdavilira@gmail.com>
2019-09-23 17:30:53 -0300

    criacao da tela de onboard e criação de classes necessárias

---
+++ Recicla Facil/Slide.swift
@@ -0,0 +1,29 @@
+//
+//  Slide.swift
+//  Recicla Facil
+//
+//  Created by aluno on 23/09/19.
+//  Copyright © 2019 aluno. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+  
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    
+    @IBOutlet weak var labelOutroTexto: UILabel!
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
